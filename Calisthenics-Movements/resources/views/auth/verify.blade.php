5546cc55fdb013ebfa341602bb5ff4793cfd1853
Guilherme Freitas <guilhermefreitas@primelan.com.br>
2021-05-16 21:12:40 -0300

    revision

--- Calisthenics-Movements/resources/views/auth/verify.blade.php
+++ Calisthenics-Movements/resources/views/auth/verify.blade.php
@@ -18,7 +18,7 @@
                     {{ __('If you did not receive the email') }},
                     <form class="d-inline" method="POST" action="{{ route('verification.resend') }}">
                         @csrf
-                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another yaaauh') }}</button>.
+                        <button type="submit" class="btn btn-link p-0 m-0 align-baseline">{{ __('click here to request another') }}</button>.
                     </form>
                 </div>
             </div>
